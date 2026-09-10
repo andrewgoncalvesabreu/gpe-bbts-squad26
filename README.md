6d07a36c0e727805b622ec0fb749824e4e609021
Andrew Gonçalves Abreu <andrewgoncalvesabreu20@gmail.com>
2026-09-10 11:18:42 -0300

    feat: estrutura inicial do projeto com Spring Boot, React Vite e Docker

---
+++ README.md
@@ -0,0 +1,1 @@
+"# SIGES-PES / GPE - Sistema de GestÆo de Parcerias Estrat‚gicas (BBTS)" 
---
+++ gpe-backend/src/main/java/br/com/bbts/gpe/gpe_backend/GpeBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbts.gpe.gpe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpeBackendApplication.class, args);
+	}
+
+}
